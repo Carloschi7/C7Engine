a8002e6fa3874e8adc4a76ff69ad3c3906777d25
carloschi7 <carloschi23@libero.it>
2023-10-17 12:33:33 +0200

    Minor adjustments

--- build.bat
+++ build.bat
@@ -4,9 +4,10 @@ REM Visual Studio build
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-mkdir debug && cd debug
+mkdir build
+pushd build
 cmake ..
 msbuild C7Engine.sln
-cd ..
+popd
 
 
