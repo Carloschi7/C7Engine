fcd564819b7e897e408301af0603d089ba79be4e
carloschi7 <carloschi23@libero.it>
2025-04-13 18:04:24 +0200

    made class Entity obsolete, move it temporarily to the gfx namespace to avoid collisions

--- engine/Entity.h
+++ engine/Entity.h
@@ -7,51 +7,56 @@
 #include "Shader.h"
 #include "VertexManager.h"
 
-class Entity // The actual vertices match the drawing of a perfect centered octagon between a -1.0f to 1.0f index range, if it's needed an orthographical representation of the obj, please scale it
+
+namespace gfx
 {
-public:
-	Entity();
-	Entity(const VertexManager& vm);
-	Entity(VertexManager&& vm);
-	Entity(const Entity&) = delete;
-	Entity(Entity&& e) noexcept;
-	~Entity();
-
-	void Draw(Shader& shd);
-	/*Requires a 3° layout in the vertex shader to gain access to the vec3 offset variable
-	EXAMPLE OF USAGE:
-	layout(location = 0) in vec3 pos;
-	(other code)
-	layout(location = 3) in vec3 offsets;
-
-	int main()
+	//INFO: @C7 Obsolete, not really useful anymore
+	class Entity
 	{
+	public:
+		Entity();
+		Entity(const VertexManager& vm);
+		Entity(VertexManager&& vm);
+		Entity(const Entity&) = delete;
+		Entity(Entity&& e) noexcept;
+		~Entity();
+
+		void Draw(Shader& shd);
+		/*Requires a 3° layout in the vertex shader to gain access to the vec3 offset variable
+		EXAMPLE OF USAGE:
+		layout(location = 0) in vec3 pos;
 		(other code)
-		gl_Position = projection * view * ((model * vec4(pos, 1.0f)) + vec4(offsets,1.0f));
-		(other code)
-	}
+		layout(location = 3) in vec3 offsets;
 
-	*/
-	void DrawInstancedPositions(Shader& shd, u32 num_instances, glm::vec3* positions);
+		int main()
+		{
+			(other code)
+			gl_Position = projection * view * ((model * vec4(pos, 1.0f)) + vec4(offsets,1.0f));
+			(other code)
+		}
 
-	void SetVertexManager(const VertexManager& vm);
-	void Rotate(f32 fRadians, const glm::vec3& dir);
-	void Translate(const glm::vec3& dir);
-	void Scale(const glm::vec3& dir);
-	void Scale(f32 fScaleFactor);
-	void ResetPosition();
+		*/
+		void DrawInstancedPositions(Shader& shd, u32 num_instances, glm::vec3* positions);
 
-	bool IsIntersectedBy(const glm::vec3& pos, const glm::vec3& dir, f32 fRadius, f32 ratio_vertex_center);
+		void SetVertexManager(const VertexManager& vm);
+		void Rotate(f32 fRadians, const glm::vec3& dir);
+		void Translate(const glm::vec3& dir);
+		void Scale(const glm::vec3& dir);
+		void Scale(f32 fScaleFactor);
+		void ResetPosition();
 
-	const glm::mat4& ModelMatrix() const;
-	const glm::vec3& GetPosition() const { return m_Position; }
-	const VertexManager* GetVertexManager() const { return m_VertexManager; }
+		bool IsIntersectedBy(const glm::vec3& pos, const glm::vec3& dir, f32 fRadius, f32 ratio_vertex_center);
 
-private:
-	VertexManager* m_VertexManager;
-	glm::mat4 m_ModelMatrix;
-	glm::vec3 m_Position;
+		const glm::mat4& ModelMatrix() const;
+		const glm::vec3& GetPosition() const { return m_Position; }
+		const VertexManager* GetVertexManager() const { return m_VertexManager; }
 
-	//For ray collision operations
-	glm::vec3* m_Vertices;
-};
+	private:
+		VertexManager* m_VertexManager;
+		glm::mat4 m_ModelMatrix;
+		glm::vec3 m_Position;
+
+		//For ray collision operations
+		glm::vec3* m_Vertices;
+	};
+}
